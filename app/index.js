7791dae5a5ca2825a19a8203e9d9369ec120a84d
unknown <me@jojostewart.com>
2018-05-12 18:11:50 +1200

    Initial commit.

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import App from './components/App';
+import styles from './styles.css';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
